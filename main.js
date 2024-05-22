01aff9d0eecbd5de40531406a3d24e86f64136af
Issa Jaramillo <issakaterinejaramilloalvarez@gmail.com>
2024-05-22 12:29:11 -0500

    colores a la card

--- main.js
+++ main.js
@@ -1,19 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const colorToggleButton = document.getElementById("colorToggle");
-//     colorToggleButton.addEventListener("click", function() {
-//       document.body.classList.toggle("dark-mode");
-//     });
-//   });
-  
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   const colorToggleBtn = document.getElementById("colorToggle");
-//   const body = document.body;
-
-//   colorToggleBtn.addEventListener("click", function() {
-//       body.classList.toggle("dark-mode");
-//   });
-// });
 document.addEventListener("DOMContentLoaded", function() {
   const colorToggleBtn = document.getElementById("colorToggle");
   const toggleIcon = document.getElementById("toggleIcon");
