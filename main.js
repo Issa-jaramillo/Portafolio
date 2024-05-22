d430deea7be0dc55a8e98f6111c9f32da062feb1
Issa Jaramillo <issakaterinejaramilloalvarez@gmail.com>
2024-05-22 10:46:31 -0500

    darkmode

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const colorToggleButton = document.getElementById("colorToggle");
-    colorToggleButton.addEventListener("click", function() {
-      document.body.classList.toggle("dark-mode");
-    });
-  });
+// document.addEventListener("DOMContentLoaded", function() {
+//     const colorToggleButton = document.getElementById("colorToggle");
+//     colorToggleButton.addEventListener("click", function() {
+//       document.body.classList.toggle("dark-mode");
+//     });
+//   });
   
+
+document.addEventListener("DOMContentLoaded", function() {
+  const colorToggleBtn = document.getElementById("colorToggle");
+  const body = document.body;
+
+  colorToggleBtn.addEventListener("click", function() {
+      body.classList.toggle("dark-mode");
+  });
+});
